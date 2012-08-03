0679a019e390936f44238e9cdd8d828dc5c79d66
John Baughman <johnmbaughman@gmail.com>
2012-08-03 00:09:44 -0600

    Coding style changes

--- Packer.cs
+++ Packer.cs
@@ -3,70 +3,71 @@ using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Dean.Edwards {
-	public partial class Packer : Form {
 
-		public Packer() {
-			InitializeComponent();
-				}
+    public partial class Packer : Form {
 
-		private void pack_Click(object sender, System.EventArgs e) {
-			ECMAScriptPacker p = new ECMAScriptPacker((ECMAScriptPacker.PackerEncoding) Encoding.SelectedItem, fastDecode.Checked, specialChars.Checked);
-			tbResult.Text = p.Pack(tbSource.Text).Replace("\n", "\r\n");
-			bSave.Enabled = true;
-		}
+        public Packer() {
+            InitializeComponent();
+        }
 
-		private void Packer_Load(object sender, System.EventArgs e) {
-			Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.None);
-			Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.Numeric);
-			Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.Mid);
-			Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.Normal);
-			Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.HighAscii);
-			Encoding.SelectedItem = ECMAScriptPacker.PackerEncoding.Normal;
-		}
+        private void pack_Click(object sender, System.EventArgs e) {
+            ECMAScriptPacker p = new ECMAScriptPacker((ECMAScriptPacker.PackerEncoding)Encoding.SelectedItem, fastDecode.Checked, specialChars.Checked);
+            tbResult.Text = p.Pack(tbSource.Text).Replace("\n", "\r\n");
+            bSave.Enabled = true;
+        }
 
-		private void Encoding_SelectedIndexChanged(object sender, System.EventArgs e) {
-			fastDecode.Enabled = ((ECMAScriptPacker.PackerEncoding)Encoding.SelectedItem != ECMAScriptPacker.PackerEncoding.None);
-		}
+        private void Packer_Load(object sender, System.EventArgs e) {
+            Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.None);
+            Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.Numeric);
+            Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.Mid);
+            Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.Normal);
+            Encoding.Items.Add(ECMAScriptPacker.PackerEncoding.HighAscii);
+            Encoding.SelectedItem = ECMAScriptPacker.PackerEncoding.Normal;
+        }
 
-		private void llPaste_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e) {
-			tbSource.Text = (string)Clipboard.GetDataObject().GetData(typeof(string));
-		}
+        private void Encoding_SelectedIndexChanged(object sender, System.EventArgs e) {
+            fastDecode.Enabled = ((ECMAScriptPacker.PackerEncoding)Encoding.SelectedItem != ECMAScriptPacker.PackerEncoding.None);
+        }
 
-		private void llCopy_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e) {
-			Clipboard.SetDataObject(tbResult.Text, true);
-		}
+        private void llPaste_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e) {
+            tbSource.Text = (string)Clipboard.GetDataObject().GetData(typeof(string));
+        }
 
-		private void bClear_Click(object sender, System.EventArgs e) {
-			tbResult.Text = tbSource.Text = string.Empty;
-			bSave.Enabled = false;
-		}
+        private void llCopy_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e) {
+            Clipboard.SetDataObject(tbResult.Text, true);
+        }
 
-		private void bLoad_Click(object sender, System.EventArgs e) {
-			DialogResult r = ofdSource.ShowDialog(this);
-			if (r == DialogResult.OK) {
-				Stream s = ofdSource.OpenFile();
-				TextReader rd = new StreamReader(s);
-				string content = rd.ReadToEnd();
-				rd.Close();
-				s.Close();
-				Regex regex = new Regex("([^\r])(\n+)");
-				tbSource.Text = regex.Replace(content, new MatchEvaluator(changeUnixLineEndings));
-			}
-		}
+        private void bClear_Click(object sender, System.EventArgs e) {
+            tbResult.Text = tbSource.Text = string.Empty;
+            bSave.Enabled = false;
+        }
 
-		private string changeUnixLineEndings(Match match) {
-			return match.Value.Replace("\n", "\r\n");
-		}
+        private void bLoad_Click(object sender, System.EventArgs e) {
+            DialogResult r = ofdSource.ShowDialog(this);
+            if (r == DialogResult.OK) {
+                Stream s = ofdSource.OpenFile();
+                TextReader rd = new StreamReader(s);
+                string content = rd.ReadToEnd();
+                rd.Close();
+                s.Close();
+                Regex regex = new Regex("([^\r])(\n+)");
+                tbSource.Text = regex.Replace(content, new MatchEvaluator(changeUnixLineEndings));
+            }
+        }
 
-		private void bSave_Click(object sender, System.EventArgs e) {
-			DialogResult r = sfdResult.ShowDialog(this);
-			if (r == DialogResult.OK) {
-				Stream s = sfdResult.OpenFile();
-				TextWriter rd = new StreamWriter(s);
-				rd.Write(tbResult.Text);
-				rd.Close();
-				s.Close();
-			}
-		}
-	}
+        private string changeUnixLineEndings(Match match) {
+            return match.Value.Replace("\n", "\r\n");
+        }
+
+        private void bSave_Click(object sender, System.EventArgs e) {
+            DialogResult r = sfdResult.ShowDialog(this);
+            if (r == DialogResult.OK) {
+                Stream s = sfdResult.OpenFile();
+                TextWriter rd = new StreamWriter(s);
+                rd.Write(tbResult.Text);
+                rd.Close();
+                s.Close();
+            }
+        }
+    }
 }
--- ParseMaster.cs
+++ ParseMaster.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Collections;
 using System.Collections.Specialized;
+using System.Text;
+using System.Text.RegularExpressions;
 
 /*
 	ParseMaster, version 1.0 (pre-release) (2005/02/01) x4
@@ -15,236 +14,203 @@ using System.Collections.Specialized;
 	Ported to C# by Jesse Hansen, twindagger2k@msn.com
 */
 
-namespace Dean.Edwards
-{
-	/// <summary>
-	/// a multi-pattern parser
-	/// </summary>
-	public class ParseMaster 
-	{
-		// used to determine nesting levels
-		Regex GROUPS = new Regex("\\("),
-			SUB_REPLACE = new Regex("\\$"),
-			INDEXED = new Regex("^\\$\\d+$"),
-			ESCAPE = new Regex("\\\\."), 
-			QUOTE = new Regex("'"),
-			DELETED = new Regex("\\x01[^\\x01]*\\x01");
+namespace Dean.Edwards {
 
-		/// <summary>
-		/// Delegate to call when a regular expression is found.
-		/// Use match.Groups[offset + &lt;group number&gt;].Value to get
-		/// the correct subexpression
-		/// </summary>
-		public delegate string MatchGroupEvaluator(Match match, int offset);
+    /// <summary>
+    /// a multi-pattern parser
+    /// </summary>
+    public class ParseMaster {
 
-		private string DELETE(Match match, int offset)
-		{
-			return "\x01" + match.Groups[offset].Value + "\x01";
-		}
+        // used to determine nesting levels
+        private Regex GROUPS = new Regex("\\("),
+            SUB_REPLACE = new Regex("\\$"),
+            INDEXED = new Regex("^\\$\\d+$"),
+            ESCAPE = new Regex("\\\\."),
+            QUOTE = new Regex("'"),
+            DELETED = new Regex("\\x01[^\\x01]*\\x01");
 
-		private bool ignoreCase = false;
-		private char escapeChar = '\0';
+        /// <summary>
+        /// Delegate to call when a regular expression is found.
+        /// Use match.Groups[offset + &lt;group number&gt;].Value to get
+        /// the correct subexpression
+        /// </summary>
+        public delegate string MatchGroupEvaluator(Match match, int offset);
 
-		/// <summary>
-		/// Ignore Case?
-		/// </summary>
-		public bool IgnoreCase
-		{
-			get { return ignoreCase; }
-			set { ignoreCase = value; }
-		}
+        private string DELETE(Match match, int offset) {
+            return string.Format("\x01{0}\x01", match.Groups[offset].Value);
+        }
 
-		/// <summary>
-		/// Escape Character to use
-		/// </summary>
-		public char EscapeChar
-		{
-			get { return escapeChar; }
-			set { escapeChar = value; }
-		}
+        private bool ignoreCase = false;
+        private char escapeChar = '\0';
 
-		/// <summary>
-		/// Add an expression to be deleted
-		/// </summary>
-		/// <param name="expression">Regular Expression String</param>
-		public void Add(string expression)
-		{
-			Add(expression, string.Empty);
-		}
+        /// <summary>
+        /// Ignore Case?
+        /// </summary>
+        public bool IgnoreCase {
+            get { return ignoreCase; }
+            set { ignoreCase = value; }
+        }
 
-		/// <summary>
-		/// Add an expression to be replaced with the replacement string
-		/// </summary>
-		/// <param name="expression">Regular Expression String</param>
-		/// <param name="replacement">Replacement String. Use $1, $2, etc. for groups</param>
-		public void Add(string expression, string replacement)
-		{
-			if (replacement == string.Empty)
-				add(expression, new MatchGroupEvaluator(DELETE));
+        /// <summary>
+        /// Escape Character to use
+        /// </summary>
+        public char EscapeChar {
+            get { return escapeChar; }
+            set { escapeChar = value; }
+        }
 
-			add(expression, replacement);
-		}
+        /// <summary>
+        /// Add an expression to be replaced with the replacement string
+        /// </summary>
+        /// <param name="expression">Regular Expression String</param>
+        /// <param name="replacement">Replacement String. Use $1, $2, etc. for groups</param>
+        public void Add(string expression, string replacement = "") {
+            if (string.IsNullOrEmpty(replacement))
+                add(expression, new MatchGroupEvaluator(DELETE));
 
-		/// <summary>
-		/// Add an expression to be replaced using a callback function
-		/// </summary>
-		/// <param name="expression">Regular expression string</param>
-		/// <param name="replacement">Callback function</param>
-		public void Add(string expression, MatchGroupEvaluator replacement)
-		{
-			add(expression, replacement);
-		}
+            add(expression, replacement);
+        }
 
-		/// <summary>
-		/// Executes the parser
-		/// </summary>
-		/// <param name="input">input string</param>
-		/// <returns>parsed string</returns>
-		public string Exec(string input)
-		{
-			return DELETED.Replace(unescape(getPatterns().Replace(escape(input), new MatchEvaluator(replacement))), string.Empty);
-			//long way for debugging
-			/*input = escape(input);
-			Regex patterns = getPatterns();
-			input = patterns.Replace(input, new MatchEvaluator(replacement));
-			input = DELETED.Replace(input, string.Empty);
-			return input;*/
-		}
+        /// <summary>
+        /// Add an expression to be replaced using a callback function
+        /// </summary>
+        /// <param name="expression">Regular expression string</param>
+        /// <param name="replacement">Callback function</param>
+        public void Add(string expression, MatchGroupEvaluator replacement) {
+            add(expression, replacement);
+        }
 
-		ArrayList patterns = new ArrayList();
-		private void add(string expression, object replacement)
-		{
-			Pattern pattern = new Pattern();
-			pattern.expression = expression;
-			pattern.replacement = replacement;
-			//count the number of sub-expressions
-			// - add 1 because each group is itself a sub-expression
-			pattern.length = GROUPS.Matches(internalEscape(expression)).Count + 1;
+        /// <summary>
+        /// Executes the parser
+        /// </summary>
+        /// <param name="input">input string</param>
+        /// <returns>parsed string</returns>
+        public string Exec(string input) {
+            return DELETED.Replace(unescape(getPatterns().Replace(escape(input), new MatchEvaluator(replacement))), string.Empty);
+            //long way for debugging
+            /*input = escape(input);
+            Regex patterns = getPatterns();
+            input = patterns.Replace(input, new MatchEvaluator(replacement));
+            input = DELETED.Replace(input, string.Empty);
+            return input;*/
+        }
 
-			//does the pattern deal with sup-expressions?
-			if (replacement is string && SUB_REPLACE.IsMatch((string) replacement))
-			{
-				string sreplacement = (string) replacement;
-				// a simple lookup (e.g. $2)
-				if (INDEXED.IsMatch(sreplacement))
-				{
-					pattern.replacement = int.Parse(sreplacement.Substring(1)) - 1;
-				}
-			}
+        private ArrayList patterns = new ArrayList();
 
-			patterns.Add(pattern);
-		}
+        private void add(string expression, object replacement) {
+            Pattern pattern = new Pattern();
+            pattern.expression = expression;
+            pattern.replacement = replacement;
+            //count the number of sub-expressions
+            // - add 1 because each group is itself a sub-expression
+            pattern.length = GROUPS.Matches(internalEscape(expression)).Count + 1;
 
-		/// <summary>
-		/// builds the patterns into a single regular expression
-		/// </summary>
-		/// <returns></returns>
-		private Regex getPatterns()
-		{
-			StringBuilder rtrn = new StringBuilder(string.Empty);
-			foreach (object pattern in patterns)
-			{
-				rtrn.Append(((Pattern) pattern).ToString() + "|");
-			}
-			rtrn.Remove(rtrn.Length - 1, 1);
-			return new Regex(rtrn.ToString(), ignoreCase ? RegexOptions.IgnoreCase : RegexOptions.None );
-		}
+            //does the pattern deal with sup-expressions?
+            if (replacement is string && SUB_REPLACE.IsMatch((string)replacement)) {
+                string sreplacement = (string)replacement;
+                // a simple lookup (e.g. $2)
+                if (INDEXED.IsMatch(sreplacement)) {
+                    pattern.replacement = int.Parse(sreplacement.Substring(1)) - 1;
+                }
+            }
 
-		/// <summary>
-		/// Global replacement function. Called once for each match found
-		/// </summary>
-		/// <param name="match">Match found</param>
-		private string replacement(Match match)
-		{
-			int i = 1, j = 0;
-			Pattern pattern;
-			//loop through the patterns
-			while (!((pattern = (Pattern) patterns[j++]) == null))
-			{
-				//do we have a result?
-				if (match.Groups[i].Value != string.Empty)
-				{
-					object replacement = pattern.replacement;
-					if (replacement is MatchGroupEvaluator)
-					{
-						return ((MatchGroupEvaluator) replacement)(match, i);
-					}
-					else if (replacement is int)
-					{
-						return match.Groups[(int) replacement + i].Value;
-					}
-					else
-					{
-						//string, send to interpreter
-						return replacementString(match, i, (string) replacement, pattern.length);
-					}
-				}
-				else //skip over references to sub-expressions
-					i += pattern.length;
-			}
-			return match.Value; //should never be hit, but you never know
-		}
+            patterns.Add(pattern);
+        }
 
-		/// <summary>
-		/// Replacement function for complicated lookups (e.g. Hello $3 $2)
-		/// </summary>
-		private string replacementString(Match match, int offset, string replacement, int length)
-		{
-			while (length > 0)
-			{
-				replacement = replacement.Replace("$" + length--, match.Groups[offset + length].Value);
-			}
-			return replacement;
-		}
-		
-		private StringCollection escaped = new StringCollection();
+        /// <summary>
+        /// builds the patterns into a single regular expression
+        /// </summary>
+        /// <returns></returns>
+        private Regex getPatterns() {
+            StringBuilder rtrn = new StringBuilder(string.Empty);
+            foreach (object pattern in patterns) {
+                rtrn.Append(string.Format("{0}|", ((Pattern)pattern).ToString()));
+            }
+            rtrn.Remove(rtrn.Length - 1, 1);
+            return new Regex(rtrn.ToString(), ignoreCase ? RegexOptions.IgnoreCase : RegexOptions.None);
+        }
 
-		//encode escaped characters
-		private string escape(string str)
-		{
-			if (escapeChar == '\0')
-				return str;
-			Regex escaping = new Regex("\\\\(.)");
-			return escaping.Replace(str, new MatchEvaluator(escapeMatch));
-		}
+        /// <summary>
+        /// Global replacement function. Called once for each match found
+        /// </summary>
+        /// <param name="match">Match found</param>
+        private string replacement(Match match) {
+            int i = 1, j = 0;
+            Pattern pattern;
+            //loop through the patterns
+            while (!((pattern = (Pattern)patterns[j++]) == null)) {
+                //do we have a result?
+                if (match.Groups[i].Value != string.Empty) {
+                    object replacement = pattern.replacement;
+                    if (replacement is MatchGroupEvaluator) {
+                        return ((MatchGroupEvaluator)replacement)(match, i);
+                    }
+                    else if (replacement is int) {
+                        return match.Groups[(int)replacement + i].Value;
+                    }
+                    else {
+                        //string, send to interpreter
+                        return replacementString(match, i, (string)replacement, pattern.length);
+                    }
+                }
+                else //skip over references to sub-expressions
+                    i += pattern.length;
+            }
+            return match.Value; //should never be hit, but you never know
+        }
 
-		private string escapeMatch(Match match)
-		{
-			escaped.Add(match.Groups[1].Value);
-			return "\\";
-		}
+        /// <summary>
+        /// Replacement function for complicated lookups (e.g. Hello $3 $2)
+        /// </summary>
+        private string replacementString(Match match, int offset, string replacement, int length) {
+            while (length > 0) {
+                replacement = replacement.Replace(string.Format("${0}", length--), match.Groups[offset + length].Value);
+            }
+            return replacement;
+        }
 
-		//decode escaped characters
-		private int unescapeIndex = 0;
-		private string unescape(string str)
-		{
-			if (escapeChar == '\0')
-				return str;
-			Regex unescaping = new Regex("\\" + escapeChar);
-			return unescaping.Replace(str, new MatchEvaluator(unescapeMatch));
-		}
+        private StringCollection escaped = new StringCollection();
 
-		private string unescapeMatch(Match match)
-		{
-			return "\\" + escaped[unescapeIndex++];
-		}
+        //encode escaped characters
+        private string escape(string str) {
+            if (escapeChar == '\0')
+                return str;
+            Regex escaping = new Regex("\\\\(.)");
+            return escaping.Replace(str, new MatchEvaluator(escapeMatch));
+        }
 
-		private string internalEscape(string str)
-		{
-			return ESCAPE.Replace(str, "");
-		}
+        private string escapeMatch(Match match) {
+            escaped.Add(match.Groups[1].Value);
+            return "\\";
+        }
 
-		//subclass for each pattern
-		private class Pattern
-		{
-			public string expression;
-			public object replacement;
-			public int length;
+        //decode escaped characters
+        private int unescapeIndex = 0;
 
-			public override string ToString()
-			{
-				return "(" + expression + ")";
-			}
-		}
-	}
+        private string unescape(string str) {
+            if (escapeChar == '\0')
+                return str;
+            Regex unescaping = new Regex("\\" + escapeChar);
+            return unescaping.Replace(str, new MatchEvaluator(unescapeMatch));
+        }
+
+        private string unescapeMatch(Match match) {
+            return "\\" + escaped[unescapeIndex++];
+        }
+
+        private string internalEscape(string str) {
+            return ESCAPE.Replace(str, "");
+        }
+
+        //subclass for each pattern
+        private class Pattern {
+            public string expression;
+            public object replacement;
+            public int length;
+
+            public override string ToString() {
+                return string.Format("({0})", expression);
+            }
+        }
+    }
 }
